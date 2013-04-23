d38ce28a78bcf85ce2431155df188444225b8960
david <revacuate@revacuate-desktop.(none)>
2013-04-23 02:13:33 -0700

    Refactor last(?) unneeded temporary variables

--- lib/message.rb
+++ lib/message.rb
@@ -10,18 +10,16 @@ class Message
     def other_kernels
       kernels = Cernel.find_kernels
       other_kernels = kernels[:all] - kernels[:installed]
-      if other_kernels.length > 0
-        ["",
-         "### NOTE: You have kernels in your /boot directory " +
-         "that have no corresponding packages installed.",
-         "###       If you know you don't want those kernels, " +
-         "you may want to remove them.",
-         "###       You can list and remove them with the following commands:",
-         list_and_remove_commands(other_kernels),
-         "", ""].join("\n")
-      else
-        String.new
-      end
+      other_kernels.length > 0 ?
+        ( ["",
+           "### NOTE: You have kernels in your /boot directory " +
+           "that have no corresponding packages installed.",
+           "###       If you know you don't want those kernels, " +
+           "you may want to remove them.",
+           "###       You can list and remove them with the following commands:",
+           list_and_remove_commands(other_kernels),
+           "", ""].join("\n") ) :
+        ( String.new )
     end
 
     def purge_packages_success(kernels_to_remove)
@@ -38,23 +36,23 @@ class Message
     end
 
     def confirm_kernels_to_be_removed(kernels_to_remove, installed_kernels)
-      string = String.new
-      kernels_to_remove.length > 1 ? (
-        string << "The #{kernels_to_remove.length} kernels marked with asterisks will be apt-get purged:\n") : (
-        string << "The kernel marked with asterisks will be apt-get purged:\n")
-      installed_kernels.each do |kernel|
-        kernels_to_remove.include?(kernel) ? (string << "**#{kernel}**\n") : (string << "  #{kernel}  \n")
-      end
-      string << "Are you sure you want to continue [y/N/yes/NO/?]"
+      [(kernels_to_remove.length > 1 ?
+         ( "The #{kernels_to_remove.length} kernels marked with asterisks will be apt-get purged:" ) :
+         ( "The kernel marked with asterisks will be apt-get purged:" )),
+       (installed_kernels.collect { |kernel|
+          kernels_to_remove.include?(kernel) ? "**#{kernel}**" : "  #{kernel}  " }), "",
+        "Are you sure you want to continue [y/N/yes/NO/?]"].join("\n")
     end
 
     private
     { "list" => "ls -ahl", "remove" => "rm -f  " }.each do |name, command|
       define_method "#{name}_command" do |other_kernels|
-        other_kernels.each_with_index.collect { |kernel, index|
+        #other_kernels.enum_for(:each_with_index).collect do |kernel, index| ## ruby-1.8.x compatible ?
+        other_kernels.each_with_index.collect do |kernel, index|
           index + 1 == other_kernels.length ?
             ( "sudo #{command} /boot/*-#{kernel}*" ) :
-            ( "sudo #{command} /boot/*-#{kernel}* && " ) }.unshift("###       `").push("`").join
+            ( "sudo #{command} /boot/*-#{kernel}* && " )
+        end.unshift("###       `").push("`").join
       end
     end
 
@@ -63,14 +61,13 @@ class Message
     end
 
     def kernel_count(installed_kernels)
-      count = installed_kernels.length
-      case
-      when count == 0
+      case installed_kernels.length
+      when 0
         "ERROR: No kernels found in the /boot directory!"
-      when count == 1
+      when 1
         "Only one kernel found!"
-      when count >= 2
-        "Found #{count} kernels installed:"
+      when 2..(1.0/0.0)
+        "Found #{installed_kernels.length} kernels installed:"
       end
     end
   end
