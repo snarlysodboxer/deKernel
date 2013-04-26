266585156b5693cf3b1478f3ad95d3f381370209
david <revacuate@revacuate-desktop.(none)>
2013-04-25 21:47:47 -0700

    Part way through revision

--- lib/deKernel.rb
+++ lib/deKernel.rb
@@ -6,7 +6,13 @@ class DeKernel
       $stdout.puts "\n" + "      THIS IS A DRY-RUN!, apt-get will only pretend." + "\n" + "\n"
     end
     $stdout.puts "It's generally recommended to leave at least three of your latest kernels installed." + "\n" + "\n"
-    Cernel.purge_packages_from_a_list_of_kernels(Cernel.ask_which_to_remove)
+    if $options[:all_except] != nil
+      Cernel.purge_packages_from_a_list_of_kernels(Cernel.find_all_except_latest($options[:all_except].to_i))
+    elsif $options[:kernels_list] != nil
+      Cernel.purge_packages_from_a_list_of_kernels($options[:kernels_list].split(" "))
+    else
+      Cernel.purge_packages_from_a_list_of_kernels(Cernel.ask_which_to_remove)
+    end
   ensure
     $stdout.puts Message.other_kernels
     $stdout.puts "#{Cernel.get_free_disk_space - original_free_space} megabytes of disk space were freed."
